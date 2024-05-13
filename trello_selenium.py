f5faecc08bb7c2a5f63b1438978ce5de216c9c16
白子敬 <zijjing@baizijingdeMacBook-Air.local>
2024-05-14 01:01:01 +0800

    updateCard

--- trello_selenium.py
+++ trello_selenium.py
@@ -36,7 +36,50 @@ driver.implicitly_wait(10)
 
 # 登入
 driver.find_element(By.CLASS_NAME, "css-178ag6o").click()
+driver.implicitly_wait(10)
 
+# 建立看板
+driver.find_element(By.CLASS_NAME, "board-tile").click()
+driver.find_element(By.CLASS_NAME, "nch-textfield__input").send_keys("test")
+time.sleep(1)
+driver.find_element(By.CLASS_NAME, "hY6kPzdkHFJhfG").click()
+driver.implicitly_wait(10)
+
+# 新增列表
+driver.find_element(By.CLASS_NAME, "frrHNIWnTojsww.CSwccJ0PrMROzz").click()
+elemL = driver.find_element(By.CLASS_NAME, "oe8RymzptORQ7h")
+elemL.send_keys("hello1")
+driver.implicitly_wait(10)
+elemL.send_keys(Keys.ENTER)
+driver.implicitly_wait(10)
+
+# 新增卡片
+driver.find_element(By.CLASS_NAME, "O9vivwyDxMqo3q.bxgKMAm3lq5BpA").click()
+elemC = driver.find_element(By.CLASS_NAME, "qJv26NWQGVKzI9")
+elemC.send_keys("card1")
+driver.implicitly_wait(10)
+elemC.send_keys(Keys.ENTER)
+time.sleep(2)
+
+# 複製卡片到hello列表
+driver.find_element(By.CLASS_NAME, "o0opcYgoysGMA4.bxgKMAm3lq5BpA").click()
+time.sleep(3)
+driver.find_element(By.CLASS_NAME, "button[data-testid='quick-card-editor-move']").click()
+time.sleep(5)
+
+# 無法定位
+driver.find_element(By.X, "js-autofocus").send_keys("card2")
+driver.implicitly_wait(10)
+elemU = driver.find_element(By.CLASS_NAME, "js-select-list")
+driver.implicitly_wait(10)
+elemU.click()
+elemU.send_keys(Keys.DOWN)
+elemU.send_keys(Keys.DOWN)
+elemU.send_keys(Keys.DOWN)
+elemU.send_keys(Keys.ENTER)
+driver.find_element(By.CLASS_NAME, "nch-button").click()
+driver.implicitly_wait(10)
+driver.find_element(By.CLASS_NAME, "css-snhnyn").click()
 
 time.sleep(10)
 # 關閉瀏覽器
